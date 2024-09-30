9825eddc8f3391392b42d16599824753a0f89450
Adnan Javed <112625274+Adnan546@users.noreply.github.com>
2024-09-30 23:50:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+    const taskInput = document.getElementById('new-task');
+    const taskList = document.getElementById('task-list');
+
+    if (taskInput.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+
+        li.addEventListener('click', function() {
+            li.classList.toggle('completed');
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function() {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
